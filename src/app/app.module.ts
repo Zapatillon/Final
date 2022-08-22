5fa45cfc07fa1baefd482ec3b7677682ca7af08b
Zapatillon <nicoschwam@gmail.com>
2022-08-22 19:33:32 -0300

    commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { TrabajoComponent } from './components/trabajo/trabajo.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SobreMiComponent,
+    SkillComponent,
+    TrabajoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
